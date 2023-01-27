0c1147cde22828e0399ccd2ee1ad79965b9fbc8c
adrian <xwrobel02@gmail.com>
2023-01-27 01:19:00 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import PostsList from "./features/posts/postsList";
+import AddPostForm from "./features/posts/AddPostForm";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
+import Layout from "./components/Layout";
+import { Route, Routes, Navigate } from "react-router-dom";
+import EditPostForm from "./features/posts/EditPostForm";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
+        </Route>
+        <Route path='user'>
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+        <Route path='*' element={<Navigate to="/" replace />}/>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
